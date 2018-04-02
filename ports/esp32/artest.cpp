19214d233a3ae2edb68e96b5a081a66b6379e10d
saurabhjadhav1911 <saurabhjadhav1911@gmail.com>
2018-04-03 02:05:59 +0530

    cpp compiling

--- ports/esp32/artest.cpp
+++ ports/esp32/artest.cpp
@@ -1,4 +1,22 @@
 //file: main.cpp
-#include "Arduino.h"
+#include <Arduino.h>
+#include "rom/gpio.h"
+#include "esp_log.h"
+#include "esp_spi_flash.h"
 
-void fun();
+#include "py/runtime.h"
+#include "py/mperrno.h"
+#include "py/mphal.h"
+#include "drivers/dht/dht.h"
+#include "modesp.h"
+int funn()
+{
+	return(4);
+}
+
+extern "C"{
+	STATIC mp_obj_t esp_fun(void) {
+	    return mp_obj_new_int_from_uint(funn());
+	}
+	STATIC MP_DEFINE_CONST_FUN_OBJ_0(esp_fun_obj, esp_fun);
+}
